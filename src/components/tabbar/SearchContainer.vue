d31167c9dbd72264f70de43bda0f76962570a81b
ilmxml <ilmxml@163.com>
2020-05-18 22:18:33 +0800

    商城前端

--- src/components/tabbar/SearchContainer.vue
+++ src/components/tabbar/SearchContainer.vue
@@ -1,12 +1,22 @@
 <template>
   <div>
-    <h3>SearchContainer</h3>
+    <mt-search v-model="value" cancel-text="取消" placeholder="搜索"></mt-search>
   </div>
 </template>
 
 <script>
+import { Search } from "mint-ui";
+import Vue from 'vue';
+
+Vue.component(Search.name, Search);
+export default {
+  data() {
+    return {
+      value: ""
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
